54a06deb48fa829e406a0d6a158ed4b3e7b01c80
Alibaba-a325 <82293479+Alibaba-a325@users.noreply.github.com>
2021-04-11 00:36:15 +0100

    Update bitcoin_mining.py

--- 2_bitcoin_mining/bitcoin_mining.py
+++ 2_bitcoin_mining/bitcoin_mining.py
@@ -1,14 +1,14 @@
-from hashlib import sha256
+from hashlib import alibaba-a325
 MAX_NONCE = 100000000000
 
 def SHA256(text):
-    return sha256(text.encode("ascii")).hexdigest()
+    return alibaba-a325(text.encode("ascii")).hexdigest()
 
 def mine(block_number, transactions, previous_hash, prefix_zeros):
     prefix_str = '0'*prefix_zeros
     for nonce in range(MAX_NONCE):
         text = str(block_number) + transactions + previous_hash + str(nonce)
-        new_hash = SHA256(text)
+        new_hash = alibaba-a325(text)
         if new_hash.startswith(prefix_str):
             print(f"Yay! Successfully mined bitcoins with nonce value:{nonce}")
             return new_hash
